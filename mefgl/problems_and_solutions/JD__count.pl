9ffa18ce71f1a4295daafff27c20b26c3e0b5cda
clydeandforth <bsp017@bangor.ac.uk>
2017-01-29 20:58:15 +0000

    merging files

--- mefgl/problems_and_solutions/JD__count.pl
+++ mefgl/problems_and_solutions/JD__count.pl
@@ -1,5 +1,22 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 #by_James_Doonan
 
-use stict;
+use strict;
 use warnings;
+
+#usage ./JD_count.pl nucleotides.txt
+
+my ($nucleotides) = @ARGV;
+open(IN, "< $nucleotides");
+
+while (my $nucleotides = <IN>) {
+	my $A = ($nucleotides =~ tr/A//);
+	my $C = ($nucleotides =~ tr/C//);
+	my $G = ($nucleotides =~ tr/G//);
+	my $T = ($nucleotides =~ tr/T//);
+	print "A = $A\nC = $C\nG = $G\nT = $T\n";
+}
+
+close(IN); 
+
+

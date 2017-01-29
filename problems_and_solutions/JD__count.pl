74d37a131c5e13755a5d8a0ac47918e219d806e3
clydeandforth <bsp017@bangor.ac.uk>
2017-01-29 20:47:26 +0000

    final solution

--- problems_and_solutions/JD__count.pl
+++ problems_and_solutions/JD__count.pl
@@ -1,5 +1,25 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 #by_James_Doonan
 
-use stict;
+use strict;
 use warnings;
+
+#usage ./JD_count.pl nucleotides.txt
+
+my ($A, $C, $G, $T) ;
+
+my ($nucleotides) = @ARGV;
+open(IN, "< $nucleotides");
+
+while (my $nucleotides = <IN>) {
+	my $A = ($nucleotides =~ tr/A//);
+	my $C = ($nucleotides =~ tr/C//);
+	my $G = ($nucleotides =~ tr/G//);
+	my $T = ($nucleotides =~ tr/T//);
+	print "A = $A\nC = $C\nG = $G\nT = $T\n";
+}
+
+close(IN); 
+
+#print ("A=$A\n", "C=$C\n", "G=$G\n", "T=$T\n");
+

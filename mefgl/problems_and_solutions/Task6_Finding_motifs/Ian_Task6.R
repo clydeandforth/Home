a41fde3f026ed1a7c0d5365a5e928bec7f993f77
iwwilson <i.wilson@bangor.ac.uk>
2017-04-11 10:26:50 +0100

    Update Ian_Task6.R

--- mefgl/problems_and_solutions/Task6_Finding_motifs/Ian_Task6.R
+++ mefgl/problems_and_solutions/Task6_Finding_motifs/Ian_Task6.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 ## Before running, requires:
 
 ## install.packages(stringr)
@@ -6,7 +8,7 @@
 library(stringr)
 
 ## Read in file containing string on 1st line and substring on 2nd line
-seqs <- file("task6_data.txt","r")
+seqs <- file('stdin', 'r');
 
 ## Make variables containing contents of each line
 string <- readLines(seqs,n=1)

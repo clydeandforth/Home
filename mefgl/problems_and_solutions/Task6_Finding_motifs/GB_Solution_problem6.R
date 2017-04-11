b4e3683e0f3cce0dcd3a0433404ca2ec6829aff3
G-Brennan <g.l.brennan@bangor.ac.uk>
2017-04-11 10:49:49 +0100

    Update GB_Solution_problem6.R

--- mefgl/problems_and_solutions/Task6_Finding_motifs/GB_Solution_problem6.R
+++ mefgl/problems_and_solutions/Task6_Finding_motifs/GB_Solution_problem6.R
@@ -1,9 +1,32 @@
 
-source("http://bioconductor.org/biocLite.R")
+## try http:// if https:// URLs are not supported
+# you only need to download it one
+source("https://bioconductor.org/biocLite.R")
+biocLite("Biostrings")
+
+# in the future all you need is to load the package
 library("Biostrings")
 
-
+# save the ss sequqnece as a string and give it a name, here I called it DNA
 DNA<-"TTCTCTTTTCTCTTTTCTCTTTTCTCTTTGTTCTCTTTTCTCTTTTCTCTTATTCTCTTATTCTCTTGGTTTTTCTCTTCTAGGTTCTCTTTTCTCTTGCAAAACTCCGGGGTTCTCTTTTCTCTTGTTTCTCTTTTCTCTTTTTCTCTTATTCTCTTTGATTCTCTTACGCTTCTCTTTGATTCTCTTCGATGTTTCTCTTGTTTCTCTTATTTCTCTTTTCTCTTGTTCTCTTTTCTCTTCAGATTCTACTTTCTCTTGTTCTCTTTTCTCTTTTCTCTTCGTTATTTCTCTTGTGTATTCTCTTTAACATTTCTCTTCTTCTCTTTTTCTCTTAATGGACATGCTTTCTCTTTTCTCTTTTCTCTTGTGACTTCTTTTTCTCTTATTCTCTTTTCTCTTAAGCTTCTCTTCATTCTCTTTTTCTCTTGTTCTCTTTCCTGTTTCTCTTGCTTCTCTTTTCTCTTTTCTCTTATCAATTCTCTTATTTTCTCTTCAGTTCTCTTCAAGCGAGGGTGCCTTCTCTTTGGGTTCTCTTCGTTCTCTTAATTTCTTCTCTTCTTTCTCTTTTTCTCTTACTTCTCTTGGGTGGTTCTCTTCCTCGACAGTGACTGGGACAAGTTCTCTTCTTCTCTTTTTCTCTTGCTGTCACCCTTCTCTTCATTCTCTTATTCTCTTACTTTCTCTTGATATTCTCTTTTCTCTTCTTCTCTTGATTCTCTTGATTCTCTTTTCTCTTCTTCTCTTCCTTCTCTTTGCCTCAGTTTCTCTTTTCTCTTTTCTCTTGTTCTCTTCGTTCTCTTTAGACATTCTCTTAACGTTCTCTTGGCGCCTAAGCGAGCCACTTCTCTTATCACTTGATTTCTCTT"
+str(DNA)
 
-
+# use matchPatter (from Biostring package) to find the tt string in the ss (DNA) string 
 matchPattern("TTCTCTTTT", DNA) 
+
+# In order to print the list of all start locations of tt I found  vmatchPatter (from Biostring package) more useful
+# vmatchPatter produces an MIndex, which can be use with startIndex, below. 
+# see ?matchPattern for more information of both functions
+result<-vmatchPattern("TTCTCTTTT", DNA) 
+
+# print results to screen
+result
+
+# prints a list containing the starting positions of the matches for each patter
+startIndex(result)
+
+# prints a list containing the ending positions of the matches for each pattern
+endIndex(result)
+
+# prints an integer vector containing the number of matches for each pattern
+elementNROWS(result)
